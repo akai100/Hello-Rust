1eb41aa75b0321dfe443588a1c88bdaccc6abd37
Xianglong Zhao <1107971906@qq.com>
2023-12-04 21:44:01 +0800

    Create main.rs

---
+++ projects/how-to-write-code-with-no-std/src/main.rs
@@ -0,0 +1,16 @@
+#![no_std]
+#![no_main]
+#![windows_subsystem = "windows"]
+
+use core::panic::PanicInfo;
+
+/// 这个函数将在 panic 时被调用
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
